2abcd8f8df360d237cbc0b7047367bb798300b25
nikolaystanishev <stanishev.nikolay@gmail.com>
2017-10-30 21:00:29 +0200

    Refactor class

--- data_processing.py
+++ data_processing.py
@@ -6,85 +6,80 @@ import csv
 import pickle
 
 from settings import image_size, pixel_depth, color_channels,\
-    number_of_train_images, number_of_test_images
+    name_of_info_file
 
 
 class DataProcessing:
     """
-    Class for processing and loading it to pickle file
+    Class for processing data and loading it to pickle file
     """
 
     def __init__(self, train_folder, test_folder):
         self.train_folder = train_folder
         self.test_folder = test_folder
 
-    def get_training(self):
-        images = np.ndarray(shape=(number_of_train_images, image_size,
-                                   image_size, color_channels),
-                            dtype=np.float32)
-        labels = np.ndarray(number_of_train_images, dtype=np.int32)
-        num_images = 0
-        image_categories_dir = os.listdir(self.train_folder)
-        for directory in image_categories_dir:
-            directory_path = os.path.join(self.train_folder, directory)
-            image_files = os.listdir(directory_path)
-            for image_filename in image_files:
-                image_path = os.path.join(directory_path, image_filename)
-                try:
-                    image_data = (ndimage.imread(image_path).astype(float) -
-                                  (pixel_depth / 2)) / pixel_depth
-                    if image_data.shape != (image_size, image_size):
-                        image = imresize(image_data,
-                                         size=(image_size, image_size))
-                    else:
-                        image = image_data
-                    images[num_images, :, :, :] = image
-                    labels[num_images] = int(directory)
-                    num_images = num_images + 1
-                except OSError as e:
-                    pass
-        images = images[0:num_images, :, :, :]
-        labels = labels[0:num_images]
-        return {'train_data': images, 'train_labels': labels}
-
-    def get_test(self):
-        images = np.ndarray(shape=(number_of_test_images, image_size,
-                                   image_size, color_channels),
-                            dtype=np.float32)
-        labels = np.ndarray(number_of_train_images, dtype=np.int32)
-        num_images = 0
-        image_files = os.listdir(self.test_folder)
+    def get_info_for_images(self, dirpath):
         images_info = {}
-        with open(os.path.join(self.test_folder, 'images_info.csv'),
+        with open(os.path.join(dirpath, name_of_info_file),
                   'rt') as cf:
             reader = csv.reader(cf)
             next(reader, None)
             images_info = {row[0].split(';')[0]: row[0].split(';')[-1]
                            for row in reader}
-        for image_filename in image_files:
-            image_path = os.path.join(self.test_folder, image_filename)
-            try:
-                image_data = (ndimage.imread(image_path).astype(float) -
-                              (pixel_depth / 2)) / pixel_depth
-                if image_data.shape != (image_size, image_size):
-                    image = imresize(image_data, size=(image_size, image_size))
-                else:
-                    image = image_data
-                images[num_images, :, :, :] = image
-                labels[num_images] = int(images_info[image_filename])
-                num_images = num_images + 1
-            except OSError as e:
-                pass
-        images = images[0:num_images, :, :, :]
-        labels = labels[0:num_images]
-        return {'test_data': images, 'test_labels': labels}
+        return images_info
+
+    def get_images_files_path(self, dirpath, filenames):
+        filenames.remove(name_of_info_file)
+        image_files = map(lambda filename: os.path.join(dirpath, filename),
+                          filenames)
+        return image_files
+
+    def process_image(self, image_file):
+        image_data = (ndimage.imread(image_file).astype(float) -
+                      (pixel_depth / 2)) / pixel_depth
+        if image_data.shape != (image_size, image_size):
+            image = imresize(image_data, size=(image_size, image_size))
+        else:
+            image = image_data
+        image = np.expand_dims(image, axis=0)
+        return image
+
+    def get_images_and_labels(self, image_files, images_info):
+        images = np.ndarray(shape=(0, image_size, image_size, color_channels),
+                            dtype=np.float32)
+        labels = np.ndarray(0, dtype=np.int32)
+
+        for image_file in image_files:
+            image = self.process_image(image_file)
+            label = int(images_info[os.path.basename(image_file)])
+            images = np.concatenate((images, image))
+            labels = np.append(labels, label)
+        return images, labels
+
+    def get_images_data_from_path(self, path):
+        images = np.ndarray(shape=(0, image_size, image_size, color_channels),
+                            dtype=np.float32)
+        labels = np.ndarray(0, dtype=np.int32)
+
+        for dirpath, dirnames, filenames in os.walk(path):
+            if not dirnames:
+                images_info = self.get_info_for_images(dirpath)
+                image_files = self.get_images_files_path(dirpath, filenames)
+                images_segment, labels_segment =\
+                    self.get_images_and_labels(image_files, images_info)
+                images = np.concatenate((images, images_segment))
+                labels = np.append(labels, labels_segment)
+        return images, labels
 
     def pickle_data(self, pickled_name):
         dataset = {}
-        train = self.get_training()
-        test = self.get_test()
-        dataset.update(train)
-        dataset.update(test)
+        train_data, train_labels =\
+            self.get_images_data_from_path(self.train_folder)
+        test_data, test_labels =\
+            self.get_images_data_from_path(self.test_folder)
+        dataset.update({'train_data': train_data,
+                        'train_labels': train_labels})
+        dataset.update({'test_data': test_data, 'test_labels': test_labels})
         with open(pickled_name, 'wb') as f:
             pickle.dump(dataset, f, pickle.HIGHEST_PROTOCOL)
 
