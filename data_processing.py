76c21e80b592398b024853143734656c3ca44700
nikolaystanishev <stanishev.nikolay@gmail.com>
2017-11-23 11:04:53 +0200

    Add some changings for new dataset Change settings file with config file. Change getting image info.

--- data_processing.py
+++ data_processing.py
@@ -2,48 +2,77 @@ import os
 import numpy as np
 from scipy import ndimage
 from scipy.misc import imresize
-import csv
+import xml.etree.ElementTree as ET
 import pickle
 from PIL import Image
 from datetime import datetime
-
-from settings import pixel_depth, color_channels, name_of_info_file
+import json
 
 
 class DataProcessing:
     """
-    Class for processing data and loading it to pickle file
+        Class for processing data and loading it to pickle file
     """
 
-    def __init__(self, train_folder, test_folder):
-        self.train_folder = train_folder
-        self.test_folder = test_folder
-        self.image_size = self.get_average_image_size(self.train_folder)
+    def __init__(self, config):
+        self.train_folder = config['dataset']['dataset_images']['train_folder']
+        self.validation_folder =\
+            config['dataset']['dataset_images']['validation_folder']
+        self.test_folder = config['dataset']['dataset_images']['train_folder']
+
+        self.train_annotations =\
+            config['dataset']['dataset_annotations']['train_annotations']
+        self.validation_annotations =\
+            config['dataset']['dataset_annotations']['validation_annotations']
+        self.test_annotations =\
+            config['dataset']['dataset_annotations']['train_annotations']
+
+        self.image_size = config['image_info']['image_size']
+        self.pixel_depth = config['image_info']['pixel_depth']
+        self.color_channels = config['image_info']['color_channels']
+
         self.time = None
 
     def pickle_data(self, pickled_name):
         start_time = datetime.now()
-        dataset = {}
-        train_data, train_labels =\
-            self.get_images_data_from_path(self.train_folder)
-        test_data, test_labels =\
-            self.get_images_data_from_path(self.test_folder)
-        dataset.update({'train_data': train_data,
-                        'train_labels': train_labels})
-        dataset.update({'test_data': test_data, 'test_labels': test_labels})
+
+        dataset = self.get_dataset()
+
         with open(pickled_name, 'wb') as f:
             pickle.dump(dataset, f, pickle.HIGHEST_PROTOCOL)
+
         end_time = datetime.now()
         self.time = end_time - start_time
 
-    def get_images_data_from_path(self, path):
+    def get_dataset(self):
+        dataset = {}
+
+        train_data, train_labels =\
+            self.get_images_data_from_path(self.train_folder,
+                                           self.train_annotations)
+        validation_data, validation_labels =\
+            self.get_images_data_from_path(self.validation_folder,
+                                           self.validation_annotations)
+        test_data, test_labels =\
+            self.get_images_data_from_path(self.test_folder,
+                                           self.test_annotations)
+
+        dataset.update({'train_data': train_data,
+                        'train_labels': train_labels})
+        dataset.update({'validation_data': validation_data,
+                        'validation_labels': validation_labels})
+        dataset.update({'test_data': test_data, 'test_labels': test_labels})
+
+        return dataset
+
+    def get_images_data_from_path(self, path, annotations):
         images = np.ndarray(shape=(0, self.image_size, self.image_size,
-                                   color_channels), dtype=np.float32)
+                                   self.color_channels), dtype=np.float32)
         labels = np.ndarray(0, dtype=np.int32)
 
         for dirpath, dirnames, filenames in os.walk(path):
             if not dirnames:
-                images_info = self.get_info_for_images(dirpath)
+                images_info = self.get_info_for_images(annotations)
                 image_files = self.get_images_files_path(dirpath, filenames)
                 images_segment, labels_segment =\
                     self.get_images_and_labels(image_files, images_info)
@@ -51,25 +80,33 @@ class DataProcessing:
                 labels = np.append(labels, labels_segment)
         return images, labels
 
-    def get_info_for_images(self, dirpath):
+    def get_info_for_images(self, annotations):
         images_info = {}
-        with open(os.path.join(dirpath, name_of_info_file),
-                  'rt') as cf:
-            reader = csv.reader(cf)
-            next(reader, None)
-            images_info = {row[0].split(';')[0]: row[0].split(';')[-1]
-                           for row in reader}
+
+        for annotation in annotations:
+            tree = ET.parse(annotation)
+            root = tree.getroot()
+            for child in root:
+                image_name = child.find('image').find('name').text
+                image_center =\
+                    (int(child.find('annorect').find('objpos').find('x').text),
+                     int(child.find('annorect').find('objpos').find('y').text))
+                image_rect = ((int(child.find('annorect').find('x1').text),
+                               int(child.find('annorect').find('y1').text)),
+                              (int(child.find('annorect').find('x2').text),
+                               int(child.find('annorect').find('y2').text)))
+                images_info[image_name] = (image_center, image_rect)
+
         return images_info
 
     def get_images_files_path(self, dirpath, filenames):
-        filenames.remove(name_of_info_file)
         image_files = map(lambda filename: os.path.join(dirpath, filename),
                           filenames)
         return image_files
 
     def get_images_and_labels(self, image_files, images_info):
         images = np.ndarray(shape=(0, self.image_size, self.image_size,
-                                   color_channels), dtype=np.float32)
+                                   self.color_channels), dtype=np.float32)
         labels = np.ndarray(0, dtype=np.int32)
 
         for image_file in image_files:
@@ -81,7 +118,7 @@ class DataProcessing:
 
     def process_image(self, image_file):
         image_data = (ndimage.imread(image_file).astype(float) -
-                      (pixel_depth / 2)) / pixel_depth
+                      (self.pixel_depth / 2)) / self.pixel_depth
         if image_data.shape != (self.image_size, self.image_size):
             image = imresize(image_data, size=(self.image_size,
                                                self.image_size))
@@ -94,11 +131,11 @@ class DataProcessing:
         resolutions = []
         for dirpath, dirnames, filenames in os.walk(path):
             if not dirnames:
-                filenames.remove(name_of_info_file)
                 for filename in filenames:
                     img = Image.open(os.path.join(dirpath, filename))
                     resolutions.append(img.size)
         mean = int(np.mean(resolutions))
+        print('Mean Image Size: {}'.format(mean))
         return mean
 
     def get_time(self):
@@ -106,6 +143,10 @@ class DataProcessing:
 
 
 if __name__ == '__main__':
-    dp = DataProcessing('./dataset/train_images', './dataset/test_images')
+    with open('./config.json') as config_file:
+        config = json.load(config_file)
+
+    dp = DataProcessing(config)
     dp.pickle_data('./dataset/dataset.pickle')
+
     print('Taken time: {}'.format(str(dp.get_time())))
