bd420ec1a073fbc669e8942f0e78eec67bfe042a
nikolaystanishev <stanishev.nikolay@gmail.com>
2017-12-29 16:25:27 +0200

    Add normalization to predict

--- predict.py
+++ predict.py
@@ -1,6 +1,6 @@
 from scipy import ndimage
 import numpy as np
-from scipy.misc import imresize
+from skimage.transform import resize
 import json
 import os
 import imghdr
@@ -22,28 +22,20 @@ class Predict:
         self.train_folder = config['dataset']['dataset_images']['train_folder']
         self.validation_folder =\
             config['dataset']['dataset_images']['validation_folder']
-        self.test_folder = config['dataset']['dataset_images']['train_folder']
+        self.test_folder = config['dataset']['dataset_images']['test_folder']
 
-        self.optimizers = {
-            '<class \'keras.optimizers.SGD\'>': './model/03/model.h5',
-            '<class \'keras.optimizers.RMSprop\'>': './model/04/model.h5',
-            '<class \'keras.optimizers.Adagrad\'>': './model/05/model.h5',
-            '<class \'keras.optimizers.Adadelta\'>': './model/06/model.h5',
-            '<class \'keras.optimizers.Adam\'>': './model/07/model.h5',
-            '<class \'keras.optimizers.Adamax\'>': './model/08/model.h5',
-            '<class \'keras.optimizers.Nadam\'>': './model/09/model.h5',
-            '<class \'keras.optimizers.SGD\'> with changed params':
-                './model/10/model.h5'}
+        self.models = {
+            '33': './model/33/model.h5'}
 
     def predict(self, image_file):
         image = self.get_image_from_file(image_file)
 
         predict = self.network.predict(image)
+
         return predict
 
     def get_image_from_file(self, image_file):
-        image_data = (ndimage.imread(image_file).astype(float) -
-                      (self.pixel_depth / 2)) / self.pixel_depth
+        image_data = ndimage.imread(image_file).astype(float)
 
         if len(image_data.shape) != self.color_channels:
             image_data = np.repeat(image_data[:, :, np.newaxis],
@@ -51,21 +43,26 @@ class Predict:
 
         if image_data.shape != (self.image_size, self.image_size,
                                 self.color_channels):
-            image = imresize(image_data, size=(self.image_size,
-                                               self.image_size))
+            image = resize(image_data,
+                           output_shape=(self.image_size,
+                                         self.image_size),
+                           mode='constant')
         else:
             image = image_data
 
         image = np.expand_dims(image, axis=0)
+
+        image = self.normalize_image_from_0_to_1(image)
+
         return image
 
-    def make_predictions_for_optimizers(self):
-        for optimizer, model_file in self.optimizers.items():
+    def make_predictions_for_models(self):
+        for model, model_file in self.models.items():
             start_time = datetime.now()
 
             self.network.load_model_file(model_file)
             print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
-            print(optimizer)
+            print(model)
             print('==========================================================')
             print('Train')
             print('==========================================================')
@@ -98,10 +95,28 @@ class Predict:
 
                 prediction = self.predict(image_file)
 
+                if np.sum(prediction) != 0:
+                    print('.')
+
                 for el in prediction[0]:
                     for e in el:
                         if np.sum(e) != 0:
                                 print(e)
+
+    def normalize_image_from_minus1_to_1(self, image):
+        normalized_image = (image - (self.pixel_depth / 2)) / self.pixel_depth
+
+        return normalized_image
+
+    def normalize_image_from_0_to_1(self, image):
+        normalized_image = image / self.pixel_depth
+
+        return normalized_image
+
+    def normalize_image_without_normalization(self, image):
+        normalized_image = image
+
+        return normalized_image
 
 
 if __name__ == '__main__':
@@ -110,4 +125,4 @@ if __name__ == '__main__':
 
     predict = Predict(config)
 
-    predict.make_predictions_for_optimizers()
+    predict.make_predictions_for_models()
