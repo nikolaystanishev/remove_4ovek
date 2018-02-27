cd3f63643df9d0515b7d73921d9e72a930915e83
nikolaystanishev <stanishev.nikolay@gmail.com>
2018-02-27 02:37:17 +0200

    Change name of VideoProcessing to VideoToImage

--- web/Aovek/video_processing.py
+++ web/Aovek/video_processing.py
@@ -2,9 +2,9 @@ from django.conf import settings
 
 import json
 
-from aovek.video.video_processing import VideoProcessing
+from aovek.video.video_to_image import VideoToImage
 
 with open(settings.CONFIG_FILE) as c_f:
     config = json.load(c_f)
 
-video_processing = VideoProcessing(config)
+video_processing = VideoToImage(config)
