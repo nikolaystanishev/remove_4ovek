466a2e426b5bcb11bf8533e6b8c6021b3fe7d4e1
nikolaystanishev <stanishev.nikolay@gmail.com>
2018-03-12 23:37:36 +0200

    Change setting path for config

--- web/Aovek/video_processing.py
+++ web/Aovek/video_processing.py
@@ -7,4 +7,8 @@ from aovek.video.video_to_image import VideoToImage
 with open(settings.CONFIG_FILE) as c_f:
     config = json.load(c_f)
 
+config['network']['model_binary_data_file'] =\
+    settings.PROJECT_ROOT + '/../../' +\
+    config['network']['model_binary_data_file']
+
 video_processing = VideoToImage(config)
