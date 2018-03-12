98372059d6a3c62a30d57909d543762ec7f07bfe
Nikolay Stanishev <stanishev.nikolay@gmail.com>
2018-03-12 23:50:13 +0200

    Add appending path for google cloud platform

--- web/web/wsgi.py
+++ web/web/wsgi.py
@@ -8,8 +8,12 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
+
+sys.path.append('/opt/bitnami/apps/django/django_projects/remove_4ovek/web')
+os.environ.setdefault("PYTHON_EGG_CACHE", "/opt/bitnami/apps/django/django_projects/remove_4ovek/web/egg_cache")
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "web.settings")
 
